e38de3203be1f3265973fd54a2b5155c91571867
Jose Eduardo Rojas Jimenez <joseeduardox@gmail.com>
2026-04-08 13:49:25 -0600

    style: fix formatting in download-model utility

--- src/bin/download_model.rs
+++ src/bin/download_model.rs
@@ -3,7 +3,8 @@ use fastembed::{EmbeddingModel, InitOptions, TextEmbedding};
 
 fn main() {
     println!("Downloading AllMiniLML6V2 model...");
-    let mut opts = InitOptions::new(EmbeddingModel::AllMiniLML6V2).with_show_download_progress(true);
+    let mut opts =
+        InitOptions::new(EmbeddingModel::AllMiniLML6V2).with_show_download_progress(true);
     opts = opts.with_cache_dir(std::path::PathBuf::from("models"));
     let _ = TextEmbedding::try_new(opts).expect("Failed to download model");
     println!("Model downloaded successfully!");
